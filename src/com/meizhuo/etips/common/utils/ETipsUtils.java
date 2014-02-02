d94c11e542b1b69f4b24cc0b89ff87899153f4dc
Jayin Ton <tonjayin@gmail.com>
2014-02-02 15:09:02 +0800

    remove dirty codes

--- src/com/meizhuo/etips/common/utils/ETipsUtils.java
+++ src/com/meizhuo/etips/common/utils/ETipsUtils.java
@@ -71,17 +71,6 @@ public class ETipsUtils {
 	}
 
 	/**
-	 * 获取当前周数 </br> 
-	 * 挖了个小坑：在SharedPreference里面，里面的SharedPreference的Current_week</br>
-	 * 实际上是保存当前week of year 而不是真的是一学期的当前周数 这样做的原因就是难以方便计算 当然 这里获取当前周数是封装好的
-	 * @deprecated
-	 * @param c
-	 * @return 当前周数
-	 */
-//	public static int getCurrentWeek1(Context c) {
-//		return SharedPreferenceHelper.getCurrentWeek(c);
-//	}
-	/**
 	 * 获取当前周数<br>
 	 * currentWeek 保存真的是当前，本学期的周数。因为currentWeek只会递增(除非你自己修改了)<br>
      * currentWeek 递增的依据，所保存的weekOfYear与当前CalendarManager.getWeekOfYeah()“不一致”就会增一(考虑到一旦跨年了，getWeekOfYeah又会从1开始)<br>

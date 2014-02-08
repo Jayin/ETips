a161f6e711ebbea29a7ce1cf5e39360390874c49
Jayin Ton <tonjayin@gmail.com>
2014-02-09 03:53:35 +0800

    redesign the note widget;suppor resize the widget:D

--- src/com/meizhuo/etips/appwidget/NotesAppWidget.java
+++ src/com/meizhuo/etips/appwidget/NotesAppWidget.java
@@ -20,15 +20,19 @@ import android.widget.TextView;
 import com.meizhuo.etips.activities.Notes;
 import com.meizhuo.etips.activities.NotesEdit;
 import com.meizhuo.etips.activities.R;
- 
+
+import com.meizhuo.etips.common.CalendarUtils;
 import com.meizhuo.etips.common.ETipsContants;
+import com.meizhuo.etips.common.L;
 import com.meizhuo.etips.common.SP;
 import com.meizhuo.etips.common.StringUtils;
 import com.meizhuo.etips.model.MNotes;
+
 /**
  * 便签桌面小插件
+ * 
  * @author Jayin Ton
- *
+ * 
  */
 public class NotesAppWidget extends AppWidgetProvider {
 	private List<MNotes> list = null;
@@ -50,63 +54,90 @@ public class NotesAppWidget extends AppWidgetProvider {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		
-		if (intent.getAction().equals(ETipsContants.Action_Notes) || intent.getAction().equals("android.appwidget.action.APPWIDGET_UPDATE")) {
-			reflush(context);
-		}else{
+
+		if (intent.getAction().equals(ETipsContants.Action_Notes)
+				|| intent.getAction().equals(
+						"android.appwidget.action.APPWIDGET_UPDATE")) {
+			// 考虑起始位为0
+			int item_no = intent.getIntExtra("item_no", -1);
+			reflush(context, item_no);
+		} else {
 			super.onReceive(context, intent);
 		}
 	}
-    
+
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
+		for (int i = 0; i < appWidgetIds.length; i++) {
+			RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+					R.layout.widget_notes);
+
+			appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
+		}
+		context.sendBroadcast(new Intent(ETipsContants.Action_Notes)); // 发广播刷新
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
+
+	private String wrapData(MNotes res) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(
+				CalendarUtils.getTimeFromat(res.getTime(),
+						CalendarUtils.TYPE_ONE)).append("\n\t")
+				.append(res.getContent());
+		return sb.toString();
+	}
+
+	// 刷新
+	private void reflush(Context context, int item_no) {
 		PendingIntent startNotes = PendingIntent.getActivity(context, 0,
 				new Intent(context, Notes.class), 0);
 		PendingIntent editNotes = PendingIntent.getActivity(context, 1,
 				new Intent(context, NotesEdit.class), 0);
-		reflush(context);
-		for (int i = 0; i < appWidgetIds.length; i++) {
-			RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-					R.layout.widget_notes);
-			remoteViews.setOnClickPendingIntent(R.id.widget_notes_content,
-					startNotes);
-			remoteViews.setOnClickPendingIntent(R.id.widget_notes_btn_addNote,
-					editNotes);
-			appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
-		}
-		context.sendBroadcast(new Intent(ETipsContants.Action_Notes)); //发广播刷新
-		super.onUpdate(context, appWidgetManager, appWidgetIds);
-	}
-
-   private String wrapData(List<MNotes> res){
-	   StringBuffer sb =new StringBuffer();
-	   for( MNotes m : res){
-		   sb.append(StringUtils.getDateFormat(m.getTime(), "mm-dd")).append("\n\t").append(m.getContent()).append("\n");
-	   }
-	   return sb.toString();
-   }
-   
-   private void reflush(Context context){
 		SP sp = new SP(ETipsContants.SP_NAME_Notes, context);
 		list = null;
 		list = new ArrayList<MNotes>();
 		String content = null;
 		if (!sp.isEmpty()) {
-			list = (List<MNotes>) sp
-					.toEntityAll(ETipsContants.TYPE_SP_Notes);
-		    content = wrapData(list);
-			
-		}else{
-			content = "随时随地地，记录生活点滴";
+			list = (List<MNotes>) sp.toEntityAll(ETipsContants.TYPE_SP_Notes);
+			if (item_no != -1)
+				content = wrapData(list.get(item_no));
+			else {
+				content = wrapData(list.get(0));// first time
+				item_no = 0;
+			}
+		} else {
+			content = "随时随地，记录生活点滴";
 		}
 		RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
 				R.layout.widget_notes);
-	
-		remoteViews.setTextViewText(R.id.widget_notes_tv_content, content);
-		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-		ComponentName componentName = new ComponentName(context, NotesAppWidget.class);
-		appWidgetManager.updateAppWidget(componentName, remoteViews);
-   }
+		remoteViews.setOnClickPendingIntent(R.id.widget_notes_content,
+				startNotes);
+		remoteViews.setOnClickPendingIntent(R.id.widget_notes_btn_addNote,
+				editNotes);
 
+		if (list.size() > 1) {
+		
+			int pre = item_no - 1 == -1 ? list.size() - 1 : item_no - 1;
+			int next = item_no + 1 == list.size() ? 0 : item_no + 1;
+			PendingIntent pi_pre = PendingIntent.getBroadcast(context, 0,
+					new Intent(ETipsContants.Action_Notes).putExtra("item_no", pre), PendingIntent.FLAG_UPDATE_CURRENT);
+			PendingIntent pi_next = PendingIntent.getBroadcast(context, 0,
+					new Intent(ETipsContants.Action_Notes).putExtra("item_no", next), PendingIntent.FLAG_UPDATE_CURRENT);
+			
+			remoteViews.setOnClickPendingIntent(R.id.iv_previous,
+					pi_pre);
+			remoteViews.setOnClickPendingIntent(R.id.iv_next,
+					pi_next);
+			L.i("item_no"+item_no);
+			L.i("pre-->"+pre);
+			L.i("next-->"+next);
+		}
+		remoteViews.setTextViewText(R.id.widget_notes_tv_content, content);
+		AppWidgetManager appWidgetManager = AppWidgetManager
+				.getInstance(context);
+		ComponentName componentName = new ComponentName(context,
+				NotesAppWidget.class);
+		appWidgetManager.updateAppWidget(componentName, remoteViews);
+	}
 }

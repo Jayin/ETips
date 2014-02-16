56507083dad2f32ca81adf1c4c0da5ff7d86a232
Jayin Ton <tonjayin@gmail.com>
2014-02-17 01:41:28 +0800

    motify the library path && add `XView`

---
+++ library/lib_XView/src/com/xview/XHeaderView.java
@@ -0,0 +1,130 @@
+package com.xview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.jayinton.xview.R;
+
+/**
+ * @author Maxwin
+ * @file XHeaderView.java
+ * @create Apr 18, 2012 5:22:27 PM
+ * @description XListView's header
+ */
+public class XHeaderView extends LinearLayout {
+    private LinearLayout mContainer;
+    private ImageView mArrowImageView;
+    private ProgressBar mProgressBar;
+    private TextView mHintTextView;
+    private int mState = STATE_NORMAL;
+
+    private Animation mRotateUpAnim;
+    private Animation mRotateDownAnim;
+
+    private final int ROTATE_ANIM_DURATION = 180;
+
+    public final static int STATE_NORMAL = 0;
+    public final static int STATE_READY = 1;
+    public final static int STATE_REFRESHING = 2;
+
+    private boolean mIsFirst;
+
+    public XHeaderView(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    /**
+     * @param context
+     * @param attrs
+     */
+    public XHeaderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+    }
+
+    private void initView(Context context) {
+        // 鍒濆鎯呭喌锛岃缃笅鎷夊埛鏂皏iew楂樺害涓�
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0);
+        mContainer = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.vw_header, null);
+        addView(mContainer, lp);
+        setGravity(Gravity.BOTTOM);
+
+        mArrowImageView = (ImageView) findViewById(R.id.header_arrow);
+        mHintTextView = (TextView) findViewById(R.id.header_hint_text);
+        mProgressBar = (ProgressBar) findViewById(R.id.header_progressbar);
+
+        mRotateUpAnim = new RotateAnimation(0.0f, -180.0f, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        mRotateUpAnim.setDuration(ROTATE_ANIM_DURATION);
+        mRotateUpAnim.setFillAfter(true);
+        mRotateDownAnim = new RotateAnimation(-180.0f, 0.0f, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        mRotateDownAnim.setDuration(ROTATE_ANIM_DURATION);
+        mRotateDownAnim.setFillAfter(true);
+    }
+
+    public void setState(int state) {
+        if (state == mState && mIsFirst) {
+            mIsFirst = true;
+            return;
+        }
+
+        if (state == STATE_REFRESHING) { // 鏄剧ず杩涘害
+            mArrowImageView.clearAnimation();
+            mArrowImageView.setVisibility(View.INVISIBLE);
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else { // 鏄剧ず绠ご鍥剧墖
+            mArrowImageView.setVisibility(View.VISIBLE);
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+
+        switch (state) {
+            case STATE_NORMAL:
+                if (mState == STATE_READY) {
+                    mArrowImageView.startAnimation(mRotateDownAnim);
+                }
+                if (mState == STATE_REFRESHING) {
+                    mArrowImageView.clearAnimation();
+                }
+                mHintTextView.setText(R.string.header_hint_refresh_normal);
+                break;
+
+            case STATE_READY:
+                if (mState != STATE_READY) {
+                    mArrowImageView.clearAnimation();
+                    mArrowImageView.startAnimation(mRotateUpAnim);
+                    mHintTextView.setText(R.string.header_hint_refresh_ready);
+                }
+                break;
+
+            case STATE_REFRESHING:
+                mHintTextView.setText(R.string.header_hint_refresh_loading);
+                break;
+            default:
+        }
+
+        mState = state;
+    }
+
+    public void setVisiableHeight(int height) {
+        if (height < 0) height = 0;
+        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mContainer.getLayoutParams();
+        lp.height = height;
+        mContainer.setLayoutParams(lp);
+    }
+
+    public int getVisiableHeight() {
+        return mContainer.getHeight();
+    }
+
+}

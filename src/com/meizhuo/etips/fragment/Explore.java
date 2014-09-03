0c8f8baab8169b2fb428fe16efb4b7f19476eb35
Jayin Ton <tonjayin@gmail.com>
2014-09-03 09:04:32 +0800

    Explore is fine

--- src/com/meizhuo/etips/fragment/Explore.java
+++ src/com/meizhuo/etips/fragment/Explore.java
@@ -1,13 +1,21 @@
 package com.meizhuo.etips.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import butterknife.InjectView;
+import butterknife.OnClick;
 
+import com.meizhuo.etips.activities.LibraryMainActivity;
+import com.meizhuo.etips.activities.LifeElectricityActivity;
+import com.meizhuo.etips.activities.ManualMainActivity;
+import com.meizhuo.etips.activities.MsgCenterActivity;
+import com.meizhuo.etips.activities.Notes;
+import com.meizhuo.etips.activities.QueryEmptyClassroom;
 import com.meizhuo.etips.activities.R;
+import com.meizhuo.etips.activities.SchoolNewsMainActivity;
+import com.meizhuo.etips.activities.SubSystemLoginActivity;
 
 /**
  * 发现
@@ -24,4 +32,49 @@ public class Explore extends BaseFragment {
 	 
 		return contentView;
 	}
+	
+	@OnClick(R.id.btn_message_center)
+	public void msg_center(){
+		openActivity(MsgCenterActivity.class);
+	}
+	
+	@OnClick(R.id.btn_wyunews)
+	public void wyunews(){
+		openActivity(SchoolNewsMainActivity.class);
+	}
+	
+	@OnClick(R.id.btn_library)
+	public void library(){
+		openActivity(LibraryMainActivity.class);
+	}
+	
+	
+	@OnClick(R.id.btn_query_classroom)
+	public void query_classroom(){
+		openActivity(QueryEmptyClassroom.class);
+	}
+	
+	@OnClick(R.id.btn_check_elcecity)
+	public void check_elcecity(){
+		openActivity(LifeElectricityActivity.class);
+	}
+	
+	@OnClick(R.id.btn_notes)
+	public void notes(){
+		openActivity(Notes.class);
+	}
+	
+	@OnClick(R.id.btn_check_source)
+	public void check_source(){
+		Intent intent =new Intent(getContext(),SubSystemLoginActivity.class);
+		intent.putExtra("toWhere", "ScoreRecordActivity");
+		openActivity(intent);
+	}
+	
+	@OnClick(R.id.btn_manual)
+	public void manual(){
+		openActivity(ManualMainActivity.class);
+	}
+	
+	
 }

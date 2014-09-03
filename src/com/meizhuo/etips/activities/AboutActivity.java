537286d46dc333cb7b86c8fd109d0fa5e11ef565
Jayin Ton <tonjayin@gmail.com>
2014-09-03 09:44:27 +0800

    closeAcitivty

--- src/com/meizhuo/etips/activities/AboutActivity.java
+++ src/com/meizhuo/etips/activities/AboutActivity.java
@@ -2,19 +2,21 @@ package com.meizhuo.etips.activities;
 
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.meizhuo.etips.common.AndroidUtils;
+
 /**
  * 关于页面
+ * 
  * @author Jayin Ton
- *
+ * 
  */
 public class AboutActivity extends BaseUIActivity {
 	private TextView tv_version;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	@Override protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.acty_about);
@@ -24,20 +26,26 @@ public class AboutActivity extends BaseUIActivity {
 
 	}
 
-	@Override
-	protected void initLayout() {
+	@Override public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			closeActivity();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override protected void initLayout() {
 
 		tv_version = (TextView) _getView(R.id.tv_version);
 		try {
-			tv_version.setText( AndroidUtils.getAppVersionName(getContext()));
+			tv_version.setText(AndroidUtils.getAppVersionName(getContext()));
 		} catch (NameNotFoundException e) {
 			e.printStackTrace();
 		}
 
 	}
 
-	@Override
-	protected void initData() {
+	@Override protected void initData() {
 	}
 
 }
